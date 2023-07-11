ae47d3c3de8d06a65f6793a90f2237bc998f5598
Zarak Ansari <zarak.ansari@yahoo.com>
2023-07-11 09:47:48 +0500

    completed till 5-8

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,27 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from 'react'
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+const Blog = ({blog}) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const toggleVisibility = () => setDetailsVisible(!detailsVisible)
+
+  return (  
+      <div style={blogStyle}>
+        <p>{blog.title} - {blog.author} <button onClick={toggleVisibility}>{ detailsVisible ? 'Hide' : 'Show' }</button></p>
+        {detailsVisible && (<><p>{blog.url}</p>
+        <p>likes {blog.likes} <button>like</button></p>
+        <p>{blog.user.name}</p></>)}
+      </div>  
+    )
+}
+
 
 export default Blog
