9f2ae8fce48c894d768b4f8123565cdea2b98f91
Zarak Ansari <zarak.ansari@yahoo.com>
2023-07-13 12:42:57 +0500

    completed 5-14

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -28,12 +28,12 @@ const Blog = (props) => {
 
   return (
     <div style={blogStyle}>
-      <p>{blog.title} - {blog.author} <button onClick={toggleVisibility}>{ detailsVisible ? 'Hide' : 'Show' }</button></p>
+      <p className="blogTitleAndAuthor">{blog.title} - {blog.author} <button onClick={toggleVisibility}>{ detailsVisible ? 'Hide' : 'Show' }</button></p>
       {detailsVisible &&
         (<div>
-          <p>{blog.url}</p>
-          <p>likes {blog.likes} <button onClick={incrementLikes}>like</button></p>
-          <p>{blog.user.name}</p>
+          <p className="blogUrl">{blog.url}</p>
+          <p className="blogLikes">likes {blog.likes} <button onClick={incrementLikes}>like</button></p>
+          <p className="blogUsername">{blog.user.name}</p>
           {(blog.user.username === props.loggedInUsername) && (<button onClick={() => props.removeBlog(blog.id)}>Remove</button>)}
         </div>)}
     </div>
